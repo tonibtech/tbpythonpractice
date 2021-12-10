b6c01e476058e56226b7703f0055bac238213831
tonibenn <toniCbenn@gmail.com>
2021-12-10 18:17:55 -0500

    Added a Secret Number Game Project

--- demo.py
+++ demo.py
@@ -43,3 +43,22 @@ madlib = f"Computer programming is so {adj}! It makes me so excited all the time
 Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(madlib)
+
+#Secret Number Game
+
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again. Too low")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high")
+
+    print(f"Yay, congrats. You have guessed the number {random_number} correctly")
+
+
+guess(10)
