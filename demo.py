d3d748cfa23e602a1924c9ba0293d4bd3e3a669a
tonibenn <toniCbenn@gmail.com>
2021-12-13 16:40:14 -0500

    FizBuzz activity

--- demo.py
+++ demo.py
@@ -76,3 +76,16 @@ def pizza():
 
 
 pizza()
+
+#FizzBuzz activity (Completed w/the help of Youtube Channel "Programming w/Mosh"
+
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+print(fizz_buzz(7))
